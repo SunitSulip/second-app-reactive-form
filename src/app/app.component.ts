ed1502d38336733d557b4ff71fe76a7c1291be0e
SunitSulip <sunits@mindfiresolutions.com>
2021-03-24 20:09:44 +0530

    html and reactive form model created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder} from '@angular/forms';
+// import {FromGroup,FormControl} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'second-app';
+  constructor (private fb: FormBuilder) {}
+
+  employeeForm = this.fb.group({
+    firstName :[''],
+    middleName :[''],
+    lastName : [''],
+    gender: [''],
+    address : this.fb.group({
+      permanentAddress: [''],
+      temporaryAddress: [''],
+      state: [''],
+      country:[''],
+    }),
+    mobile:[''],
+    email:[''],
+    password: [''],
+    confirmPassword: [''],
+    captchaA: [''],
+    captchaB :[''],
+  })
+
+  
 }
