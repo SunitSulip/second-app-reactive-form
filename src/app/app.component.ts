27552d13ceaa30a9604d9a6071ffca25dceae687
SunitSulip <sunits@mindfiresolutions.com>
2021-03-25 12:07:17 +0530

    added validation and submit function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, Validators} from '@angular/forms';
+import { passwordValidator } from './app.validator';
+import { AbstractControl } from "@angular/forms";
 // import {FromGroup,FormControl} from '@angular/forms'
 @Component({
   selector: 'app-root',
@@ -7,26 +9,42 @@ import {FormBuilder} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor (private fb: FormBuilder) {}
-
+  constructor (private fb: FormBuilder) {
+    this.captchaReload();
+  }
+  public captchaA: number;
+  public captchaB: number;
+  public add:number;
+  
   employeeForm = this.fb.group({
-    firstName :[''],
+    firstName :['',[Validators.required,Validators.minLength(3)]],
     middleName :[''],
-    lastName : [''],
-    gender: [''],
+    lastName : ['',[Validators.required,Validators.minLength(3)]],
+    gender: ['',Validators.required],
     address : this.fb.group({
-      permanentAddress: [''],
+      permanentAddress: ['',Validators.required],
       temporaryAddress: [''],
-      state: [''],
-      country:[''],
+      state: ['',Validators.required],
+      country:['',Validators.required],
     }),
-    mobile:[''],
-    email:[''],
-    password: [''],
-    confirmPassword: [''],
-    captchaA: [''],
-    captchaB :[''],
-  })
+    mobile:['',[Validators.required,Validators.pattern('^\\d{10}$')]],
+    email:['',[Validators.required,Validators.pattern('[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]],
+    password: ['',Validators.required],
+    confirmPassword: ['',Validators.required],
+    captcha:['',[Validators.required]]
+  },{validator : passwordValidator});
+
+
+
+  captchaReload(){
+    this.captchaA=Math.floor(Math.random() * Math.floor(9))
+   this.captchaB=Math.floor(Math.random() * Math.floor(9))
+   this.add=this.captchaA+this.captchaB
+  }
+  submitted = false
+  onSubmit(){
+    this.submitted =true;
+  }
 
   
 }
